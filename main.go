498fa808445e251cf84f798dc9309ce5a9988a96
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-02-24 20:55:08 -0800

    feat: serve static files

--- main.go
+++ main.go
@@ -1,25 +1,23 @@
 package main
 
-
 import (
-	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
-}
-
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
 func main() {
-    http.HandleFunc("/", getRoot)
-    http.HandleFunc("/hello", getHello)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
 
-    http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
