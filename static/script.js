aae3fe5f33958a6b8e9ce89e20b3ab2bb5909868
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-02-24 22:05:54 -0800

    feat: trigger rain effect on water button click

--- static/script.js
+++ static/script.js
@@ -1,2 +1,9 @@
 // Add JavaScript code here to handle button interactions and potato animations
 
+const waterButton = document.getElementById("water-button");
+waterButton.onclick = (event) => {
+    document.body.classList.add("rain");
+    setTimeout(() => {
+        document.body.classList.remove("rain");
+    }, 2000);
+};
