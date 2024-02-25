f176cb53e22c384df15a913abf87978e61e03958
Kai Devrim <kai@devrim.tech>
2024-02-25 00:03:36 -0800

    add http basic auth

--- main.go
+++ main.go
@@ -11,13 +11,55 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-
+	http.HandleFunc("/", play)
 	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+var users = map[string]string{
+	"test": "notsecret",
+}
+
+func isAuthorised(username, password string) bool {
+	pass, ok := users[username]
+	if !ok {
+		return false
+	}
+
+	return password == pass
+}
+
+func play(w http.ResponseWriter, r *http.Request) {
+	username, password, ok := r.BasicAuth()
+	if !ok {
+		w.Header().Add("WWW-Authenticate", `Basic realm="Give username and password"`)
+		w.WriteHeader(http.StatusUnauthorized)
+		http.RedirectHandler("/register", 401)
+		return
+	}
+
+	if !isAuthorised(username, password) {
+		w.Header().Add("WWW-Authenticate", `Basic realm="Give username and password"`)
+		w.WriteHeader(http.StatusUnauthorized)
+		http.RedirectHandler("/login", 401)
+		return
+	}
+
+	http.FileServer(http.Dir("./static/")).ServeHTTP(w, r)
+}
+
+func auth() {
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+
 }
